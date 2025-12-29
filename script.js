39747b2961f0d807bed65023b154048dc6a118d0
RohanTenson <rohanfortnite13@gmail.com>
2025-12-29 16:46:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function changeWatch(style) {
+  const img = document.getElementById("mainWatch");
 
+  if (style === "green") {
+    img.src = "images/watch-green.png";
+  }
+  if (style === "silver") {
+    img.src = "images/watch-silver.png";
+  }
+  if (style === "gold") {
+    img.src = "images/watch-gold.png";
+  }
+}
